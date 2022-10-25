021e9f7057550413f7f1928f197446fce35fe92b
KARUN1421 <89479514+KARUN1421@users.noreply.github.com>
2022-10-24 19:13:11 -0500

    added files

---
+++ app/src/main/Java/com/example/internshipmanagementsystem/Faculty/IndividualListActivity.java
@@ -0,0 +1,16 @@
+package com.example.internshipmanagementsystem.faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.internshipmanagementsystem.R;
+
+public class IndividualListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_list);
+    }
+}
